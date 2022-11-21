7fbed5baf8a015840354a33d47c844fcf4de6301
Md Abdullah Al Mahmud Pias <98183257+AlMahmudPias@users.noreply.github.com>
2022-11-21 21:06:02 +0600

    all done

--- design.js
+++ design.js
@@ -32,6 +32,23 @@ function update_CaseNumber (isIncrease){
   
   return new_CaseNumber;
 }
+function update_airpodNumber (isIncrease){
+  const airpodNumberField = document.getElementById('airpod-number-field');
+  const airpodNumberString = airpodNumberField.value;
+  const previousairpodNumber = parseInt(airpodNumberString);
+
+  let newairpodNumber;
+
+  if(isIncrease){
+      newairpodNumber = previousairpodNumber + 1;
+  }
+  else{
+    newairpodNumber = previousairpodNumber - 1;
+  }
+  airpodNumberField.value = newairpodNumber;
+  
+  return newairpodNumber;
+}
 function phone_total_price(newPhoneNumber){
   const phone_total_price = 1219 * newPhoneNumber;
   const update_phone_price = document.getElementById('phone-price');
@@ -41,6 +58,11 @@ function case_total_price(newcaseNumber){
   const case_total_price = 59 * newcaseNumber;
   const update_case_price = document.getElementById('case-price');
   update_case_price.innerText = case_total_price;
+}
+function airpod_total_price(newairpodNumber){
+  const airpod_total_price = 119 * newairpodNumber;
+  const update_case_price = document.getElementById('airpod-price');
+  update_case_price.innerText = airpod_total_price;
 }
 function get(x){
   const phone_Total = document.getElementById(x);
@@ -55,7 +77,8 @@ function set(x, value){
 function update_total_price(){
   const phone_total = get('phone-price');
   const case_total = get('case-price');
-  const sub_total = phone_total + case_total;
+  const airpod_total = get('airpod-price')
+  const sub_total = phone_total + case_total + airpod_total;
   set('total-price', sub_total);
 
   const convert_tax_amount = (sub_total * 0.2).toFixed(2);
@@ -94,12 +117,14 @@ document.getElementById('btn-case-minus').addEventListener('click', function(){
   case_total_price(newcaseNumber);
   update_total_price();
 })
-
-//submit button action
-
-const loginBtn = document.getElementById("check-out-btn");
-loginBtn.addEventListener("click",function(){
-const log = document.getElementById("cart1"); 
-log.style.display= 'none';
-document.getElementById("1").style.display ="block";
+document.getElementById('btn-airpod-plus').addEventListener('click', function(){
+  const new_airpod_Number = update_airpodNumber(true);
+  airpod_total_price(new_airpod_Number);
+  update_total_price();
 })
+document.getElementById('btn-airpod-minus').addEventListener('click', function(){
+  const new_airpod_Number = update_airpodNumber(false);
+  airpod_total_price(new_airpod_Number);
+  update_total_price();
+})
+
